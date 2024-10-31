c5fc85181b5bc1eb277f340795a2d6eb6c8e096a
Polina <plskwtzkk@gmail.com>
2024-10-31 22:13:13 +0300

    SPRINT 11 COMMIT 7

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 @Slf4j
 public class InMemoryFilmStorage implements FilmStorage {
     private final Map<Long, Film> films = new HashMap<>();
+
     private long getNextId() {
         long currentMaxId = films.keySet()
                 .stream()
